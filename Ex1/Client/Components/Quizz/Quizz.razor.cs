8744cb6410ff31fdacf46a6eb5cb2fbfe6f3ad58
Person M <michael.person@cognitic.be>
2021-07-23 09:54:35 +0200

    Add project files.

---
+++ Ex1/Client/Components/Quizz/Quizz.razor.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex1.Client.Components.Quizz
+{
+    public class QuizzBase: ComponentBase
+    {
+        private int pos = 1;
+        protected string infoPos;
+        string _user;
+        readonly List<string> Questions;
+
+        [Parameter]
+        public string User
+        {
+            get
+            {
+                return _user;
+            }
+
+            set
+            {
+                _user = value;
+            }
+        }
+        [Parameter]
+        public bool IsDisabled
+        {
+            get;set;
+        }
+
+        [Parameter]
+        public EventCallback<string> onAnswer { get; set; }
+        
+        protected string currentQuestion;
+
+        public QuizzBase()
+        {
+            Questions = new List<string>() { 
+                "Blazor permet-il de faire du SPA?", 
+                "Un EventCallBack<T> fonctionne comme un .net event?",
+                "L'exercice est-il bien compris?"};
+            currentQuestion = Questions[0];
+            infoPos="voici la question n° " + pos;
+        }
+
+        public async Task Answer(string rep)
+        {
+            await onAnswer.InvokeAsync(rep);
+            pos++;
+            infoPos = "voici la question n° " + pos;
+            if (pos > Questions.Count)
+            {
+                await onAnswer.InvokeAsync($"FIN DU QUIZZ");
+                currentQuestion = "";
+                infoPos = " Vous avez terminé!";
+            }
+            else
+            {
+
+                currentQuestion = Questions[pos - 1];
+                
+            }
+        }
+
+
+
+    }
+}
---
+++ Ex1/Client/Pages/Index.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex1.Client.Pages
+{
+    public partial class  Index
+    {
+        private MarkupString _reponses;
+        private bool _IsDisabled = false;
+        private string Nom = string.Empty;
+        private Task DisplayInformations(string info)
+        {
+            _reponses = (MarkupString) (_reponses.Value+ $"<br> {info} ");
+            if (info.Contains("FIN")) _IsDisabled = true;
+            return Task.CompletedTask;
+        }
+
+       
+    }
+}
